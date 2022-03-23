a481360693fa47bb9ba1c727bfd83b1ddaae0b0d
Lee Inji <90603357+injilee@users.noreply.github.com>
2022-03-23 21:37:18 +0900

    🔨  Modify : code

--- resource/music.js
+++ resource/music.js
@@ -1,1 +1,27 @@
 'use strict';
+
+const playBtn = document.querySelector('.play');
+const stopBtn = document.querySelector('.stop');
+
+const sound = new Audio('./assets/audio/아카펠라 생일축하노래  Happy birthday song A capella.mp3');
+playBtn.addEventListener('click', start);
+stopBtn.addEventListener('click', stop);
+
+function start(){
+    playSound(sound);
+}
+
+function stop(){
+    stopSound(sound);
+}
+
+function playSound(sound){
+    sound.currentTime = 0;
+    sound.volume = 0.5;
+    sound.play();
+}
+
+function stopSound(sound){
+    sound.currentTime = 0;
+    sound.pause();
+}
